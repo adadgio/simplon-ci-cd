750b82e7b74d6efd848538c13074bf3b42926098
adadgio <romain.bruckert.esc@gmail.com>
2021-01-14 14:20:50 +0100

    - Test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+const restify = require('restify')
+
 /**
  * First automated github actions app.
  */
-console.log("My app works")
-console.log("Testing...")
+const server = restify.createServer()
+server.get("/", (req, res, next) => {
+    res.send(200, { result: "Ok" })
+    return next()
+})
+
+server.listen(8989, function () {
+    console.log('%s listening at %s', server.name, server.url);
+})
